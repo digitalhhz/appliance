db7cf62e740d688c5a249b029174d231a75a200e
Lizard <64777432+Andrew71423@users.noreply.github.com>
2021-07-03 11:04:14 +0200

    adjust to say yes to everything

--- docker.sh
+++ docker.sh
@@ -1,18 +1,19 @@
 #! /bin/sh
 echo "(1) System Updaten"
-sudo apt-get update && sudo apt-get upgrade
+sudo apt-get update -y && sudo apt-get upgrade -y
 echo "(2) Docker installieren"
 curl -sSL https://get.docker.com | sh
 echo "(3) Python installieren"
-sudo apt-get install libffi-dev libssl-dev
-sudo apt install python3-dev
+sudo apt-get install -y libffi-dev libssl-dev
+sudo apt install -y python3-dev
 sudo apt-get install -y python3 python3-pip
 echo "(4) Docker compose installieren und aktivieren"
-sudo pip3 install docker-compose
+sudo pip3 install -y docker-compose
 sudo systemctl enable docker
 echo "(5) Git installieren"
-sudo apt install git
+sudo apt install -y git
 echo "(6) Umgebung vorbereiten"
 sudo git clone https://github.com/digitalhhz/appliance.git
 echo "(7) Container hochfahren"
+cd appliance
 sudo docker-compose up -d
