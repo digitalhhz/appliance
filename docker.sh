e0c6e4d38a2fb886fe180b6a6cb4077a47fc27ff
Lizard <64777432+Andrew71423@users.noreply.github.com>
2021-08-04 21:10:51 +0200

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -4,20 +4,18 @@ curl -fsSL https://get.docker.com | sh
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
-echo "(2) System Updaten"
-sudo apt-get update -y && sudo apt-get upgrade -y
-echo "(3) Python installieren"
+echo "(2) Python installieren"
 sudo apt-get install -y libffi-dev libssl-dev
 sudo apt install -y python3-dev
 sudo apt-get install -y python3 python3-pip
-echo "(4) Docker compose installieren und aktivieren"
+echo "(3) Docker compose installieren und aktivieren"
 sudo pip3 install docker-compose
 sudo systemctl enable docker
-echo "(5) Git installieren"
+echo "(4) Git installieren"
 sudo apt install -y git
-echo "(6) Umgebung vorbereiten"
+echo "(5) Umgebung vorbereiten"
 sudo git clone https://github.com/digitalhhz/appliance.git
-echo "(7) Container hochfahren"
+echo "(6) Container hochfahren"
 cd appliance
 sudo docker volume create portainer_data
 sudo chown -R 1000:1000 ./nodered
